27be6fdf009998414ed87757d1a7556cf509d56e
Leo Funk <leo.funk1@hotmail.com>
2026-04-05 12:04:11 +0200

    Renamed gridContainer to container It is shorter but still as understandeable

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const container = document.querySelector("#gridContainer");
+const container = document.querySelector("#container");
 
 for(let i = 0; i < 16; i++) {
     const div = document.createElement("div");
